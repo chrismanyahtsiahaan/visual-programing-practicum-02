e12d9f5eaee175178fc64df6e9b47b3f1d9a2a8e
chrismanyahtsiahaan <chrismansyahsiahaan19@gmail.com>
2023-09-30 15:16:41 +0700

    12S23003 - Chrismansyah T Siahaan
    Saya Telah Menyelesaikan P02_01 dan P02_02

---
+++ P02-01.java
@@ -0,0 +1,42 @@
+// 12S23003 - Chrismansyah T Siahaan
+
+import java.util.*;
+import java.lang.Math;
+
+class P02.01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String namatugas, matakuliah, pengampu, deadline, format, status, rekomendasitugas, kodematkul;
+        int tingkatkesulitan, harihinggadeadline;
+        double prioritas;
+
+        namatugas = input.nextLine();
+        kodematkul = input.nextLine();
+        matakuliah = input.nextLine();
+        pengampu = input.nextLine();
+        deadline = input.nextLine();
+        format = input.nextLine();
+        tingkatkesulitan = input.nextInt();
+        harihinggadeadline = input.nextInt();
+        status = input.nextLine();
+        prioritas = tingkatkesulitan * (1.0 / harihinggadeadline);
+        if (prioritas > 3) {
+            rekomendasitugas = "penting! Anda harus mengerjakan tugas ini segera.";
+        } else {
+            if (prioritas < 3 && prioritas > 1.5) {
+                rekomendasitugas = "Tugas ini memiliki prioritas menengah.";
+            } else {
+                if (prioritas < 1.5) {
+                    rekomendasitugas = "Tugas ini relsatif ringan, namun jangan tunda terlalu lama.";
+                }
+            }
+        }
+        System.out.println("Prioritas : " + toFixed(prioritas,4));
+        System.out.println(namatugas + "|" + kodematkul + "|" + matakuliah + "|" + pengampu + "|" + deadline + "|" + format + "|" + tingkatkesulitan + "|" + harihinggadeadline + "|" + status);
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
---
+++ P02-02.java
@@ -0,0 +1,33 @@
+12S23003 - Chrismansyah T Siahaan
+
+import java.util.*;
+import java.lang.Math;
+
+class P02.02 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String namaGudang, aC, lantai, ketersediaanTeknologi, kualitasGudang, gudangElite, gudangStandar, gudangPerluPeningkatan;
+        int kapasitasGudang, jumlahBukuSaatIni, skor;
+
+        namaGudang = input.nextLine();
+        kapasitasGudang = input.nextInt();
+        jumlahBukuSaatIni = input.nextInt();
+        aC = input.nextLine();
+        lantai = input.nextLine();
+        ketersediaanTeknologi = input.nextLine();
+        skor = (int) ((double) jumlahBukuSaatIni / kapasitasGudang * 100);
+        if (skor >= 40) {
+            kualitasGudang = "Gudang Elite";
+        } else {
+            if (skor >= 25) {
+                kualitasGudang = "Gudang Standar";
+            } else {
+                if (skor < 25) {
+                    kualitasGudang = "Gudang Perlu Peningkatan";
+                }
+            }
+        }
+        System.out.println(namaGudang + "|" + kapasitasGudang + "|" + jumlahBukuSaatIni + "|" + aC + "|" + lantai + "|" + ketersediaanTeknologi + "|" + kualitasGudang);
+    }
+}
